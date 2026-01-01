e6c5b30ce0aaccccd2e3c5190eda1c4964e2bdf1
Cyan cyrt <akihasora123@gmail.com>
2026-01-01 19:10:56 +0700

    tambah migrasi

--- database/migrations/2026_01_01_115408_kr.php
+++ database/migrations/2026_01_01_115408_kr.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('dosen_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('mata_kuliah_id')->constrained()->onDelete('cascade');
+            $table->timestamp('claimed_at')->nullable();
             $table->timestamps();
         });
     }
