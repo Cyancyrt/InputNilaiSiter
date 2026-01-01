cde7930f835a796428095f0fa7416a7b073c4ec1
Cyan cyrt <akihasora123@gmail.com>
2026-01-01 19:00:09 +0700

    tambah migrasi

--- database/migrations/2026_01_01_115408_kr.php
+++ database/migrations/2026_01_01_115408_kr.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('krs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('dosen_id')->constrained()->onDelete('cascade');
+            $table->foreignId('dosen_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('mata_kuliah_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
